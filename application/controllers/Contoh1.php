0c8dd12b218a6bc7d65add12095f8a5de6289735
Nadineesan <nadinenissa.9h@gmail.com>
2024-07-02 22:05:25 +0700

    pustaka booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Nadia Ardine 
+             saya tinggal di daerah Prumnas Klender
+             Olahraga yang saya sukai adalah Plank";
+    }
+}
